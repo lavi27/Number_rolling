d552f7213b1ea5aa022927086112c5a23369df6f
lavi27 <62757958+lavi27@users.noreply.github.com>
2021-08-26 14:16:42 +0900

    1

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-var roll = document.getElementsByClassName("roll");
 function num_roll(count, num) {
-    for (var i=0; i<num.toString().length; i++) {
-        document.getElementById(count).innerHTML += "<div>";
-        for (var l=0; l<10; l++) {
-            document.getElementById(count).innerHTML += "<i>" + l + "</i>";
+    var target = document.getElementById("roll_" + count);
+    var childcount = target.childElementCount;
+    if(num.toString().length > childcount) {
+        for (var i=0; i<num.toString().length - childcount; i++) {
+            var content = "<div class='number' style='bottom:0px'>";
+            for (var l=0; l<10; l++) {
+                content += "<a>" + l + "</a>";
+            }
+            content += "</div>";
+            target.innerHTML += content;
         }
-        document.getElementById(count).innerHTML += "</div>";
+    } else if(num.toString().length < childcount) {
+        for(var i=0; i < childcount - num.toString().length; i++) {
+            document.getElementsByClassName("number")[target.childElementCount-1].remove();
+        }
+    }
+    target.style.height = document.getElementsByClassName("number")[0].clientHeight/10 + "px";
+    target.style.width = document.getElementsByClassName("number")[0].clientWidth*num.toString().length + "px";
+    for (var i=0; i<num.toString().length; i++) {
+        var pos = Number((num + '').split('')[i]) * (document.getElementsByClassName("number")[0].clientHeight/10);
+        document.getElementsByClassName("number")[i].style = "bottom:" + pos + "px";
     }
 }
 
+function random_num() {
+    num_roll(1, Math.floor(Math.random() * 10000000000));
+}
+
 window.addEventListener('DOMContentLoaded', function() {
-    num_roll(1, 0123456789);
+    num_roll(1, "0123456789");
 });
